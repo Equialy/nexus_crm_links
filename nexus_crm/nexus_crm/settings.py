324c9ca9a64c6148f37dadcc48b809281d3466cf
Max <qiwima@gmail.com>
2025-06-07 23:17:15 +0300

    fix: logout form

--- nexus_crm/nexus_crm/settings.py
+++ nexus_crm/nexus_crm/settings.py
@@ -139,6 +139,11 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
+
+# Login setiings
+LOGIN_REDIRECT_URL = 'users:index'
+LOGOUT_REDIRECT_URL = 'users:index'
+
 # Celery
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
 CELERY_RESULT_BACKEND =  'redis://127.0.0.1:6379'
