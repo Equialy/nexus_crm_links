e14f23b426bec8d6246ea615570f6fee27e04c40
Max <qiwima@gmail.com>
2025-06-08 14:55:46 +0300

    chore: create model clients

--- nexus_crm/clients/models.py
+++ nexus_crm/clients/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from users.models import UserProfile
+
+
 # Create your models here.
+class Clients(models.Model):
+    name = models.CharField(max_length=255,
+                            verbose_name="ФИО Клиента")
+    phone = models.CharField(max_length=16,
+                             verbose_name="Телефон")
+    phone_2 = models.CharField(max_length=16,
+                               verbose_name="Второй телефон",
+                               blank=True)
+    telegram = models.CharField(max_length=55,
+                                verbose_name="telegram",
+                                blank=True)
+    email = models.EmailField(verbose_name="email", blank=True)
+    manager = models.ForeignKey(to=UserProfile,
+                                on_delete=models.SET_NULL,
+                                related_name="clients",
+                                null=True, blank=True)
+
+    def __str__(self):
+        return f"Клиент: {self.name}"
+
+    class Meta:
+        db_table = "clients"
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+        ordering = ["-id"]
